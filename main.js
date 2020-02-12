0b1b9c3035b0daacada2ac9f26e1542eea2221c7
yusbel86 <yusbel1086@gmail.com>
2020-02-12 23:38:51 +0000

    areglando1

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-alert("Hola")
+
 
 let who = ['the dog','my granma','his turtle','my bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
  
  
- nuemro = Math.floor(Math.random() * 5);  
+ rannunwho = Math.floor(Math.random() * 4);  
+ rannunwhat = Math.floor(Math.random() * 4);  
+ rannunwhen = Math.floor(Math.random() * 5);  
 
-         document.write(nuemro); 
+ console.log(who[rannunwho]+ ' ' + what[rannunwhat]+ ' ' + when[rannunwhen]);
+         
