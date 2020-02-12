63a83c793e40bf8a4a0804b78671ce58ab7dc1ad
yusbel86 <yusbel1086@gmail.com>
2020-02-12 23:32:22 +0000

    areglando

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("Hola")
+
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+ 
+ 
+ nuemro = Math.floor(Math.random() * 5);  
+
+         document.write(nuemro); 
